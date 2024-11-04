8226ef908409f6c45e773d219bb2c6e756ef2e29
Artem Sokolov <=>
2024-11-04 23:18:24 +0300

    added deno

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import { Application } from "jsr:@oak/oak";
 
+function haha(p:string) {
+  return p
+}
 const app = new Application();
 app.use((ctx) => {
   ctx.response.headers.set("Content-Type", "text/html");
   ctx.response.body = "<body>GET request received!</body>";
+  let a: Boolean;
+  a = true;
 });
 
 app.listen({ port: 8000 });
