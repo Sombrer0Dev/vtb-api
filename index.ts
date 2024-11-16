3b13663d2901f761566bdd9b72b5d89f87afe33f
Sokolov Artem <smbrer1@gmail.com>
2024-11-16 13:38:35 +0300

    hello world (#15)
    Co-authored-by: Artem Sokolov <arsokolov@ptsecurity.com>

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Application } from "jsr:@oak/oak";
 const app = new Application();
 app.use((ctx) => {
   ctx.response.headers.set("Content-Type", "text/html");
-  ctx.response.body = "<body>Hello, World!!</body>";
+  ctx.response.body = "<body>Hello World!!</body>";
 });
 
 app.listen({ port: 8000 });
