f02e559405ecbc574855686076381f63ec2b1959
Artem Sokolov <arsokolov@ptsecurity.com>
2024-11-16 13:32:41 +0300

    hello world

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Application } from "jsr:@oak/oak";
 const app = new Application();
 app.use((ctx) => {
   ctx.response.headers.set("Content-Type", "text/html");
-  ctx.response.body = "<body>GET request received!</body>";
+  ctx.response.body = "<body>Hello, World!!</body>";
 });
 
 app.listen({ port: 8000 });
